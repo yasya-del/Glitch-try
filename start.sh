480f9f5c61febf0b50b0690c7bb38c5e944a97c2
vasya <vasiliszaitseva@yandex.ru>
2025-04-10 20:05:41 +0300

    для семестровки по инфе

--- start.sh
+++ start.sh
@@ -11,7 +11,6 @@ def return_sample_page():
                     <meta charset="utf-8">
                     <title>я хороший</title>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                   </head>
                   <body>
                     <h1>Пожалуйста, возьмите меня и Машу в IT класс</h1>
