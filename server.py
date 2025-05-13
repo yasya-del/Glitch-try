d32ff2d983b6c4bf8c5fe1d05f1bd3f25d629066
vasya <vasiliszaitseva@yandex.ru>
2025-05-13 12:57:40 +0300

    1

--- server.py
+++ server.py
@@ -1,10 +1,29 @@
 from flask import Flask, url_for, request
+import requests
 
+server_address = 'http://geocode-maps.yandex.ru/1.x/?'
+api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
+geocode = 'Красная площадь 1'
+# Готовим запрос.
+geocoder_request = f'{server_address}apikey={api_key}&geocode={geocode}&format=json'
+
+# Выполняем запрос.
+response = requests.get(geocoder_request)
+if response:
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первый топоним из ответа геокодера.
+    # Согласно описанию ответа, он находится по следующему пути:
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    # Полный адрес топонима:
+    toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
 app = Flask(__name__)
 
 
 @app.route('/<name>')
 def return_sample_page(name):
+    print(toponym_address)
     if name == 'вася':
         name = 'Машу'
     else:
